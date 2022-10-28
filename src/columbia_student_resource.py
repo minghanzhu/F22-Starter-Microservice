a12e4c43255e61d589b568211e3345c007c3124b
Minghan Zhu <mz2716@columbia.edu>
2022-10-28 03:32:35 -0400

    Update columbia_student_resource.py

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -10,14 +10,13 @@ class ColumbiaStudentResource:
 
     @staticmethod
     def _get_connection():
-
         usr = os.environ.get("DBUSER")
         pw = os.environ.get("DBPW")
         h = os.environ.get("DBHOST")
 
         conn = pymysql.connect(
-            user=usr,
-            password=pw,
+            user="root",
+            password="dbuserdbuser",
             host=h,
             cursorclass=pymysql.cursors.DictCursor,
             autocommit=True
@@ -26,7 +25,6 @@ class ColumbiaStudentResource:
 
     @staticmethod
     def get_by_key(key):
-
         sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
         conn = ColumbiaStudentResource._get_connection()
         cur = conn.cursor()
@@ -35,3 +33,12 @@ class ColumbiaStudentResource:
 
         return result
 
+    @staticmethod
+    def get_all():
+        sql = "SELECT * FROM f22_databases.columbia_students";
+        conn = ColumbiaStudentResource._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql)
+        result = cur.fetchall()
+
+        return result
